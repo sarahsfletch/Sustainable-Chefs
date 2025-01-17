7b445ac37537a10bafa1982ea6cf953bf49e4604
natalia-escobar1 <naaat007@gmail.com>
2025-01-17 14:18:45 -0500

    moving-weird

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -6,6 +6,7 @@ public class movement : MonoBehaviour
 
     //get input from player
     //apply movement to sprite
+    public Animator animator;
 
     private void Update () 
     {
@@ -14,7 +15,28 @@ public class movement : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, vertical);
 
+        AnimateMovement(direction);
+
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0) 
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+
+            }
+            else 
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
 
